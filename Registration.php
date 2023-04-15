b256bfd500fa17c198a92e219004a7d657befeab
Mustafa Mahmoud <124030775+MustafaMahmoudAbdelaleem@users.noreply.github.com>
2023-04-15 08:44:57 +0200

    Create Registration.php

---
+++ Registration.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="register.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+<h2>SIGN UP</h2>
+<p>Please fill this to create account.</p>
+            <form  method="post" action="reg_backend.php">
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">User Name</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" name = "Uname" required>
+                </div>
+               <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" name = "Upass" required>
+               </div>
+               <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" name = "Co_Upass" required>
+               </div>
+              <button type="submit" class="btn btn-primary" value="submit" name="submit">CREATE </button>
+              <p>Already have an account? <a href="./login.php">Log in here.</a></p>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
